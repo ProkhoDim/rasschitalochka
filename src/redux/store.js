a99439c5b88235dcf977cf09069b393c8b081d28
Maxim Kozin <maxa.kozin@gmail.com>
2020-10-23 12:47:41 +0300

    adding Modal component desktop version

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,39 @@
-// import storage from 'redux-persist/lib/storage';
-// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import authReducer from './auth/auth-reducers';
+import storage from 'redux-persist/lib/storage';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import authReducer from './auth/auth-reducers';
 
-// const middleware = [
-//   ...getDefaultMiddleware({
-//     serializableCheck: {
-//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//     },
-//   }),
-// ];
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
 
-// const persistStoreConfig = {
-//   key: 'bearer',
-//   storage,
-//   whitelist: ['token'],
-// };
+const persistStoreConfig = {
+  key: 'bearer',
+  storage,
+  whitelist: ['token'],
+};
 
-// const store = configureStore({
-//   reducer: {
-//     auth: persistReducer(persistStoreConfig, authReducer),
-//   },
-//   middleware,
-//   devTools: process.env.NODE_ENV === 'development',
-// });
+const store = configureStore({
+  reducer: {
+    auth: persistReducer(persistStoreConfig, authReducer),
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
 
-// const persistor = persistStore(store);
+const persistor = persistStore(store);
 
-// export default { persistor, store };
+export default { persistor, store };
