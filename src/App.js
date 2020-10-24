bb870edc5a85300c2ec23c3cfb25d8699558c97b
Stanislav Avramov <lipazaoknom@gmail.com>
2020-10-24 19:06:41 +0300

    Currency exchange

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import CurrencyExchange from './Components/CurrencyExchange';
+import getCurrencyExchangeCourse from './services/bankAPI';
 
 function App() {
-  return <div className="App"></div>;
+  const [bankData, setBankData] = useState(null);
+
+  useEffect(() => {
+    const getBankData = async () => {
+      const data = await getCurrencyExchangeCourse();
+      const filteredData = data.filter(el => el.ccy !== 'BTC');
+      return setBankData(filteredData);
+    };
+    getBankData();
+  }, []);
+
+  return (
+    <div className="App">
+      {bankData && <CurrencyExchange data={bankData} />}
+    </div>
+  );
 }
 
 export default App;
