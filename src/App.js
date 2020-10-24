8bd813d094d0c15744aa4428a1ce1e681b3170f4
Svetlana Zlydneva <svetlanaderiglazova87@gmail.com>
2020-10-24 19:22:06 +0300

    render block statistics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './fonts/fonts.css';
+import StatisticsView from './views/StatisticsView'; //когда будет роутинг удалить
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StatisticsView />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Statistics/ChartSelection/ChartSelection.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './ChartSelection.module.css';
+
+const month = [
+  { value: '01', name: 'January' },
+  { value: '02', name: 'February' },
+  { value: '03', name: 'March' },
+  { value: '04', name: 'April' },
+  { value: '05', name: 'May' },
+  { value: '06', name: 'June' },
+  { value: '07', name: 'July' },
+  { value: '08', name: 'August' },
+  { value: '09', name: 'September' },
+  { value: '10', name: 'October' },
+  { value: '11', name: 'November' },
+  { value: '12', name: 'December' },
+];
+const year = [
+  { name: '2016' },
+  { name: '2017' },
+  { name: '2018' },
+  { name: '2019' },
+  { name: '2020' },
+];
+
+const ChartSelection = () => (
+  <section className={styles.Section}>
+    <select name="month" className={styles.Select}>
+      <option value disabled selected>
+        Month
+      </option>
+      {month.map(({ value, name }) => (
+        <option key={value + name} value={value}>
+          {name}
+        </option>
+      ))}
+    </select>
+    <select name="year" className={styles.Select}>
+      <option value disabled selected>
+        Year
+      </option>
+      {year.map(({ name }) => (
+        <option key={name} value={name}>
+          {name}
+        </option>
+      ))}
+    </select>
+  </section>
+);
+
+export default ChartSelection;
