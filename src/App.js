adaf51ff970ead405cf598a1e6dc06fef266fef4
Olena Savchenko <olsavchenko1@gmail.com>
2020-10-24 12:13:44 +0300

    changed App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import AppBar from './components/AppBar';
-// import TotalBalance from './components/TotalBalance';
 import ModalBtn from './components/ModalBtn';
 import SideBar from './components/Sidebar';
-
-// function App() {
-//   // return <div className="App"></div>;
-//   <Logo />;
-// }
 
 const App = () => (
   <>
     <AppBar />
     <SideBar />
-    {/* <TotalBalance /> */}
     <ModalBtn />
   </>
 );
