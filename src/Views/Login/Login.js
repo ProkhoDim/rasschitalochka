c78656d5ddc1e6f6af82970e92925b4ba6ceed3a
Stanislav Avramov <lipazaoknom@gmail.com>
2020-10-27 23:18:54 +0200

    login view css update

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { authOperations } from '../../redux/auth';
 import routes from '../../routes';
+
+import phone from '../../assets/background/phone-mock-up.png';
 import s from './Login.module.css';
 
 const initialState = {
@@ -28,48 +30,63 @@ class Login extends Component {
     const { email, password } = this.state;
     const isBtnNotDisabled = email.length > 0 && password.length > 0;
     return (
-      <div className={s.loginBlock}>
-        <div className={s.logo}></div>
-        <span className={s.projectName}>Raschitalochka</span>
-        <form
-          className={s.loginForm}
-          onSubmit={this.onSubmitHandler}
-          autoComplete="off"
-        >
-          <label>
-            <input
-              className={s.emailInput}
-              type="email"
-              name="email"
-              placeholder="E-mail as Login"
+      <div className={s.main__container}>
+        <div className={s.desktopImage__container}>
+          <img
+            src={phone}
+            alt="mobile phone"
+            className={s.desktopImage__phone}
+          />
+        </div>
+        <div className={s.loginBlock__container}>
+          <div className={s.loginBlock}>
+            <div className={s.logo}></div>
+            <span className={s.projectName}>Raschitalochka</span>
+            <form
+              className={s.loginForm}
+              onSubmit={this.onSubmitHandler}
               autoComplete="off"
-              value={email}
-              onChange={this.onChangeHandler}
-            />
-          </label>
+            >
+              <label>
+                <input
+                  className={s.emailInput}
+                  type="email"
+                  name="email"
+                  placeholder="E-mail as Login"
+                  autoComplete="off"
+                  value={email}
+                  onChange={this.onChangeHandler}
+                />
+              </label>
 
-          <label>
-            <input
-              className={s.passwordInput}
-              type="password"
-              name="password"
-              value={password}
-              autoComplete="off"
-              placeholder="Password"
-              onChange={this.onChangeHandler}
-            />
-          </label>
-          <button
-            className={s.btnLogin}
-            type="submit"
-            disabled={!isBtnNotDisabled}
-          >
-            Enter
-          </button>
-        </form>
-        <NavLink className={s.regLink} exact to={routes.REGISTER}>
-          Register
-        </NavLink>
+              <label>
+                <input
+                  className={s.passwordInput}
+                  type="password"
+                  name="password"
+                  value={password}
+                  autoComplete="off"
+                  placeholder="Password"
+                  onChange={this.onChangeHandler}
+                />
+              </label>
+              <button
+                className={s.btnLogin}
+                type="submit"
+                disabled={!isBtnNotDisabled}
+              >
+                Enter
+              </button>
+            </form>
+            <NavLink className={s.regLink} exact to={routes.REGISTER}>
+              Register
+            </NavLink>
+          </div>
+        </div>
+
+        <p className={s.appDescription}>
+          <span>Manage your budget</span> <span>with finance app</span>
+        </p>
       </div>
     );
   }
