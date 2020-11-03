9ce7ec6cb21ca73cb84d8ed22bbff658a4aaa21c
YaroslavKuzero <49403383+YaroslavKuzero@users.noreply.github.com>
2020-11-03 17:19:34 +0200

    login to hooks/regexp mail validate

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,154 +1,137 @@
-import React, { Component } from 'react';
-import * as EmailValidator from 'email-validator';
-import { connect } from 'react-redux';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { authOperations, authSelectors } from '../../redux/auth';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { authOperations } from '../../redux/auth';
+import { Notification, Media } from '../../common';
 import * as routes from '../../constants/routes';
-
-import phone from '../../assets/images/phone-mock-up.png';
-import s from './Login.module.css';
-import { Media, Notification } from '../../common';
+import emailValidate from '../../services/emailValidate';
+import { ToastContainer } from 'react-toastify';
 import Loader from 'react-loader-spinner';
 
-const initialState = {
-  user: {
-    email: '',
-    password: '',
-  },
-  isValidEmail: '',
-};
+import 'react-toastify/dist/ReactToastify.css';
+import styles from './Login.module.css';
 
-class Login extends Component {
-  state = initialState;
+import phone from '../../assets/images/phone-mock-up.png';
 
-  componentWillUnmount() {
-    this.setState(initialState);
-  }
+export default function Login() {
+  const dispatch = useDispatch();
 
-  onChangeHandler = ({ currentTarget: { name, value } }) => {
-    this.setState(prevState => ({
-      user: {
-        ...prevState.user,
-        [name]: value,
-      },
-    }));
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isValidEmail, setIsValidEmail] = useState('');
+
+  const resetForm = () => {
+    setEmail('');
+    setPassword('');
+    setIsValidEmail('');
   };
 
-  onBlurEmailHandler = e => {
-    if (EmailValidator.validate(this.state.user.email) === true) {
-      this.setState({ isValidEmail: true });
+  const onChangeEmailHandler = e => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePasswordHandler = e => {
+    setPassword(e.target.value);
+  };
+
+  const onBlurEmailHandler = e => {
+    if (emailValidate(email)) {
+      setIsValidEmail(true);
     } else {
-      this.setState({ isValidEmail: false });
+      setIsValidEmail(false);
     }
   };
 
-  onSubmitHandler = async e => {
-    const { onLogin } = this.props;
-    e.preventDefault();
-    const { email, password } = this.state.user;
-    await onLogin({
-      email: email.toLowerCase(),
-      password,
-    });
-    const { error } = this.props;
-    if (error) return Notification('error', error, 2000);
-    return;
-  };
+  const { error } = useSelector(state => state.auth);
 
-  render() {
-    const { email, password } = this.state.user;
-    const { isLoading } = this.props;
-    const isBtnNotDisabled = this.state.isValidEmail && password.length > 4;
-    return (
-      <div className={s.main__container}>
-        {isLoading && (
-          <div style={{ position: 'absolute', top: 30, right: 30 }}>
-            <Loader
-              type="ThreeDots"
-              color="#6d6d6d"
-              height={'2rem'}
-              width={60}
-            />
-          </div>
-        )}
-        <Media device="desktop">
-          <div className={s.desktopImage__container}>
-            <img
-              src={phone}
-              alt="mobile phone"
-              className={s.desktopImage__phone}
-            />
-          </div>
-        </Media>
-        <div className={s.loginBlock__container}>
-          <div className={s.loginBlock}>
-            <div className={s.logo}></div>
-            <span className={s.projectName}>Raschitalochka</span>
-            <form
-              className={s.loginForm}
-              onSubmit={this.onSubmitHandler}
-              autoComplete="off"
-            >
-              <label>
-                <input
-                  className={s.emailInput}
-                  type="email"
-                  name="email"
-                  placeholder="E-mail as Login"
-                  autoComplete="off"
-                  value={email}
-                  onChange={this.onChangeHandler}
-                  onBlur={this.onBlurEmailHandler}
-                />
-              </label>
-              {this.state.isValidEmail === false && (
-                <div className={s.warningText}>
-                  <span>Please enter valid email address!</span>
-                </div>
-              )}
-              <label>
-                <input
-                  className={s.passwordInput}
-                  type="password"
-                  name="password"
-                  value={password}
-                  autoComplete="off"
-                  placeholder="Password"
-                  onChange={this.onChangeHandler}
-                />
-              </label>
-              <button
-                className={s.btnLogin}
-                type="submit"
-                disabled={!isBtnNotDisabled}
-              >
-                Enter
-              </button>
-            </form>
-            <NavLink className={s.regLink} exact to={routes.REGISTER}>
-              Register
-            </NavLink>
-          </div>
+  useEffect(() => {
+    Notification('error', error, 2000);
+  }, [error]);
+
+  const onSubmitHandler = useCallback(
+    event => {
+      event.preventDefault();
+      dispatch(authOperations.login({ email, password }));
+      resetForm();
+    },
+    [dispatch, email, password],
+  );
+
+  const { isLoading } = useSelector(state => state.auth);
+
+  const isBtnNotDisabled = isValidEmail && password.length > 4;
+
+  return (
+    <div className={styles.main__container}>
+      {isLoading && (
+        <div style={{ position: 'absolute', top: 30, right: 30 }}>
+          <Loader type="ThreeDots" color="#6d6d6d" height={'2rem'} width={60} />
         </div>
-
-        <p className={s.appDescription}>
-          <span>Manage your budget</span> <span>with finance app</span>
-        </p>
-        <ToastContainer />
+      )}
+      <Media device="desktop">
+        <div className={styles.desktopImage__container}>
+          <img
+            src={phone}
+            alt="mobile phone"
+            className={styles.desktopImage__phone}
+          />
+        </div>
+      </Media>
+      <div className={styles.loginBlock__container}>
+        <div className={styles.loginBlock}>
+          <div className={styles.logo}></div>
+          <span className={styles.projectName}>Raschitalochka</span>
+          <form
+            className={styles.loginForm}
+            onSubmit={onSubmitHandler}
+            autoComplete="off"
+          >
+            <label>
+              <input
+                className={styles.emailInput}
+                type="email"
+                name="email"
+                placeholder="E-mail as Login"
+                autoComplete="off"
+                value={email}
+                onChange={onChangeEmailHandler}
+                onBlur={onBlurEmailHandler}
+              />
+            </label>
+            {isValidEmail === false && (
+              <div className={styles.warningText}>
+                <span>Please enter valid email address!</span>
+              </div>
+            )}
+            <label>
+              <input
+                className={styles.passwordInput}
+                type="password"
+                name="password"
+                value={password}
+                autoComplete="off"
+                placeholder="Password"
+                onChange={onChangePasswordHandler}
+              />
+            </label>
+            <button
+              className={styles.btnLogin}
+              type="submit"
+              disabled={!isBtnNotDisabled}
+            >
+              Enter
+            </button>
+          </form>
+          <NavLink className={styles.regLink} exact to={routes.REGISTER}>
+            Register
+          </NavLink>
+        </div>
       </div>
-    );
-  }
+
+      <p className={styles.appDescription}>
+        <span>Manage your budget</span> <span>with finance app</span>
+      </p>
+      <ToastContainer />
+    </div>
+  );
 }
-
-const mapStateToProps = state => ({
-  isLoading: authSelectors.getIsLoading(state),
-  error: authSelectors.getError(state),
-});
-
-const mapDispatchToProps = dispatch => ({
-  onLogin: data => dispatch(authOperations.login(data)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
